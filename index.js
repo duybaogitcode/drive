7d2b70520a769e2ce7cfab3348a91d2a335d7eff
Duy Bao <bao1322002@gmail.com>
2023-09-07 16:05:54 +0700

    fist commit

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
-app.get('/duybao', (req, res) => {
+var CronJob = require('cron').CronJob;
+var job = new CronJob(
+  '*/1 * * * *', // Chạy mỗi 10 phút
+  async function () {
+    try {
+      const response = await axios.get('https://api.fxchange.me/', {
+        timeout: 15000, // Thời gian chờ (milliseconds)
+      });
+
+      console.log('Awake server');
+    } catch (error) {
+      if (error.code === 'ECONNABORTED') {
+        console.error('Server awake request timed out');
+      } else {
+        console.error('Error sending server awake request');
+      }
+    }
+  },
+  null,
+  true,
+  'America/Los_Angeles'
+);
+
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
